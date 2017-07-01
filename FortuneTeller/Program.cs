c96b4bdc89998b8f167fd8a3d8c9e3530e90eb55
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-01 13:41:40 -0400

    extras added to project

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -17,49 +17,182 @@ namespace FortuneTeller
                 "vacation house will be located. \nI will forsee the fortune that you " +
                 "will acquire.");
             Console.WriteLine();
-            Console.WriteLine("All that I need from you is six personal facts. These must be" +
+            Console.WriteLine("At any time in this program you may type \"Quit\" to end the session.");
+            Console.WriteLine();
+            Console.WriteLine("All that I need from you is six personal facts. These must be " +
                 "true, because I can tell if you are lying!");
             Console.WriteLine();
+            string quitter = "Nobody likes a quitter...";
+
             Console.WriteLine("To begin, What is your first name?");
             string firstName = Console.ReadLine();
             string lowerCaseFirstName = firstName.ToLower();
 
+            if(lowerCaseFirstName == "quit")
+            {
+                Console.WriteLine(quitter);
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine();
             Console.WriteLine("What is your last name?");
             string lastName = Console.ReadLine();
             string lowerCaseLastName = lastName.ToLower();
 
+            if (lowerCaseLastName == "quit")
+            {
+                Console.WriteLine(quitter);
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine();
             Console.WriteLine("What is your age? Ladies, your age will not be disclosed.");
-            int userAge = int.Parse(Console.ReadLine());
-            if (userAge <= 0)
+            string userAgeString = Console.ReadLine();
+
+            if (userAgeString == "quit")
             {
-                Console.WriteLine("You are not born yet.");
+                Console.WriteLine(quitter);
+                Environment.Exit(0);
+            }
+            int userAge;
+
+            userAge = Convert.ToInt32(userAgeString);
+
+            while (userAge <= 0 || userAge > 120)
+            {
+                if (userAge <= 0)
+                {
+                    do
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Your age has to be greater than zero \"0\".");
+                    } while (!int.TryParse(Console.ReadLine(), out userAge));
+                }
+                if (userAge > 120)
+                {
+                    do
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Are you sure?!? Most people do not live past 120.");
+                    } while (!int.TryParse(Console.ReadLine(), out userAge));
+                }
             }
 
+            Console.WriteLine();
             Console.WriteLine("What is your birth month? Please enter the 2 digit month, not the name.");
-            int userBirthMonth = int.Parse(Console.ReadLine());
+            string userBirthMonthString = Console.ReadLine();
 
-            if(userBirthMonth > 12 || userBirthMonth < 1)
+            if (userBirthMonthString == "quit")
             {
-                Console.WriteLine("You know there are only 12 months in a year.");
+                Console.WriteLine(quitter);
+                Environment.Exit(0);
+            }
+            int userBirthMonth;
+            userBirthMonth = Convert.ToInt32(userBirthMonthString);
+
+            while (userBirthMonth > 12 || userBirthMonth < 1)
+            {
+                if (userBirthMonth > 12)
+                {
+                    do
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("You know there are only 12 months in a year.");
+                    } while (!int.TryParse(Console.ReadLine(), out userBirthMonth));
+                }
+                if (userBirthMonth > 12 || userBirthMonth < 1)
+                {
+                    do
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Come on! What is a negative month?.");
+                    } while (!int.TryParse(Console.ReadLine(), out userBirthMonth));
+                }
             }
 
+            Console.WriteLine();
             Console.WriteLine("What is your favorite \"ROYGBIV\" color?  If you are not familiar " +
-                "with the term \"ROYGBIV\" please type \"Help\".");
+            "with the term \"ROYGBIV\" please type \"Help\".");
             string userFavColor = Console.ReadLine();
             string lowerCaseUserFavColor = userFavColor.ToLower();
-            
-            //This if statement is for if the user types in the word "help".
+            if (lowerCaseUserFavColor == "quit")
+            {
+                Console.WriteLine(quitter);
+                Environment.Exit(0);
+            }
+
+            while (lowerCaseUserFavColor != "red" && lowerCaseUserFavColor != "orange" &&
+                lowerCaseUserFavColor != "yellow" && lowerCaseUserFavColor != "green" &&
+                lowerCaseUserFavColor != "blue" && lowerCaseUserFavColor != "indigo" && 
+                lowerCaseUserFavColor != "violet" && lowerCaseUserFavColor != "help")
+            {
+                Console.WriteLine("Please enter a \"ROYGBIV\" color or enter \"Help\".");
+                userFavColor = Console.ReadLine();
+                lowerCaseUserFavColor = userFavColor.ToLower();
+
+                if (lowerCaseUserFavColor == "quit")
+                {
+                    Console.WriteLine(quitter);
+                    Environment.Exit(0);
+                }
+                if (lowerCaseUserFavColor.ToLower() == "help")
+                {
+                    Console.WriteLine("The following colors are \"ROYGBIV\" colors: ");
+                    Console.WriteLine("Red, Orange, Yellow, Green, Blue, Indigo, Violet.");
+                    Console.WriteLine("Please enter one of these colors.");
+                    userFavColor = Console.ReadLine();
+                    lowerCaseUserFavColor = userFavColor.ToLower();
+
+                    if (lowerCaseUserFavColor == "quit")
+                    {
+                        Console.WriteLine(quitter);
+                        Environment.Exit(0);
+                    }
+                }
+
+            }
+            if (lowerCaseUserFavColor == "quit")
+            {
+                Console.WriteLine(quitter);
+                Environment.Exit(0);
+            }
             if (lowerCaseUserFavColor.ToLower() == "help")
             {
                 Console.WriteLine("The following colors are \"ROYGBIV\" colors: ");
                 Console.WriteLine("Red, Orange, Yellow, Green, Blue, Indigo, Violet.");
                 Console.WriteLine("Please enter one of these colors.");
                 userFavColor = Console.ReadLine();
+                lowerCaseUserFavColor = userFavColor.ToLower();
+
+                while (lowerCaseUserFavColor != "red" && lowerCaseUserFavColor != "orange" &&
+                lowerCaseUserFavColor != "yellow" && lowerCaseUserFavColor != "green" &&
+                lowerCaseUserFavColor != "blue" && lowerCaseUserFavColor != "indigo" &&
+                lowerCaseUserFavColor != "violet" && lowerCaseUserFavColor != "help")
+                {
+                    Console.WriteLine("Please enter the a correct color.");
+                    userFavColor = Console.ReadLine();
+                    lowerCaseUserFavColor = userFavColor.ToLower();
+                    if (lowerCaseUserFavColor == "quit")
+                    {
+                        Console.WriteLine(quitter);
+                        Environment.Exit(0);
+                    }
+                }
             }
 
+            Console.WriteLine();
             Console.WriteLine("How many siblings do you have?");
-            int userSiblings = int.Parse(Console.ReadLine());
+            string userSiblingsString = Console.ReadLine();
 
+            if (userSiblingsString == "quit")
+            {
+                Console.WriteLine(quitter);
+                Environment.Exit(0);
+            }
+            int userSiblings;
+            userSiblings = Convert.ToInt32(userSiblingsString);
+
+            Console.WriteLine();
             Console.WriteLine("Thank you for making the right choice in utilizing my " +
                 "exquisite talents to determine your future!\n\n" +
                 "I will tell your fortune momentarily. \n\n\n");
@@ -72,7 +205,7 @@ namespace FortuneTeller
             Console.Write(firstName + " " + lastName + " will retire in ");
             if (userAge % 2 == 0)
             {
-                Console.Write("20 years "); 
+                Console.Write("20 years ");
             }
             else
             {
@@ -103,23 +236,23 @@ namespace FortuneTeller
 
             if (userSiblings == 0)
             {
-                Console.Write("a vacation home in Heavenly Hawaii ");
+                Console.Write("a vacation home in Kauai, Hawaii ");
             }
-            else if(userSiblings == 1)
+            else if (userSiblings == 1)
             {
-                Console.Write("a vacation home in Fabulous Fiji ");
+                Console.Write("a vacation home in fabulous Viti Levu, Fiji ");
             }
-            else if(userSiblings == 2)
+            else if (userSiblings == 2)
             {
-                Console.Write("a vacation home in Terrific Toronto ");
+                Console.Write("a vacation home in terrific Toronto, Canada ");
             }
-            else if(userSiblings == 3)
+            else if (userSiblings == 3)
             {
-                Console.Write("a vacation home in Lovely Los Cabos ");
+                Console.Write("a vacation home in lovely Los Cabos, Mexico ");
             }
             else
             {
-                Console.Write("a vacation home in an Insane Asylum ");
+                Console.Write("a vacation home in a foreign Insane Asylum ");
             }
 
             //The color selected will determine the user's transportation
@@ -133,25 +266,27 @@ namespace FortuneTeller
                     Console.Write("and a skateboard.\n");
                     break;
                 case "yellow":
-                    Console.Write("is a banana seat bike.\n");
+                    Console.Write("and a banana seat bike.\n");
                     break;
                 case "green":
-                    Console.Write("is a Honda Prius.\n");
+                    Console.Write("and a Honda Prius.\n");
                     break;
                 case "blue":
-                    Console.Write("is an airplane.\n");
+                    Console.Write("and a Learjet 60 airplane.\n");
                     break;
-                case "Indigo":
-                    Console.Write("is a helicopter.\n");
+                case "indigo":
+                    Console.Write("and a helicopter.\n");
                     break;
-                case "Violet":
-                    Console.Write("is horse and buggy.\n");
+                case "violet":
+                    Console.Write("and a horse and buggy.\n");
                     break;
                 default:
                     Console.Write("and you do not have any transportation.\n");
-                    break;                
+                    break;
             }
             Console.WriteLine();
+            Console.WriteLine("________________");
         }
-    }
+    }        
 }
+
